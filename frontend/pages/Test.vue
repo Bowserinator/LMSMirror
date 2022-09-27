b16fa51a610bad538224d535a11a65c76b1efc6c
MeiH10 <huangm10@rpi.edu>
2022-09-27 15:04:17 -0400

    implemented basic navigation bar with logo and a few other links
    implemented basic navigation bar with logo and a few other links. This bar should appear on all the pages.

---
+++ frontend/pages/Test.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <p class="text">test</p>
+        asdasd
+    </div>
+</template>
+  
+<script>
+export default {
+    name: 'TestPage'
+}
+</script>
+
+<style scoped>
+.text { color: red; font-weight: bold; }
+</style>
+  
