7a593f07ade6c5ffbf29f311c0077b0a7acd073e
PrestonWaters <prestoncwaters@gmail.com>
2022-10-18 16:48:28 -0400

    A class to contain functions to grab various json files. Requires all implementation.

---
+++ backend/flaskr/API_Wrapper.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 18 16:28:18 2022
+
+@author: Preston Waters
+"""
+
+"""
+https://developer.blackboard.com/portal/displayApi
+
+
+https://stackoverflow.com/questions/12965203/how-to-get-json-from-webpage-into-python-script
+or use Preston's old code that grabs a .json file
+if you're fine using beautifulSoup
+"""
+
+# This should be functions that return json files for various things
+
+
+class apiWrapper(object):
+
+    # Okay so what do I need this to do
+
+    print('temp')
+
+    # Function to grab
+
+
+if __name__ == '__main__':  # personal testing code
+
+    raise Exception("No tests here buster")
