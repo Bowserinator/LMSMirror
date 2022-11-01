70c456648470fcb2667e8f8c473fd0bf7ff60c36
MeiH10 <huangm10@rpi.edu>
2022-10-31 21:23:46 -0400

    created the layout of the single course page by using grid and card to create various boxes for certain content.

--- frontend/pages/singleCoursePage.vue
+++ frontend/pages/singleCoursePage.vue
@@ -16,17 +16,58 @@
                     </v-btn>
                 </v-flex>
 
-
-
             </v-layout>
 
 
         </v-container>
-        <!-- <PageHeader text="Linear Algebra" />
-        <v-btn>
-            <v-icon left>mdi-refresh</v-icon>
-            <span class="text-capitalize">Refresh</span>
-        </v-btn> -->
+
+
+        <v-container>
+            <v-layout row wrap>
+
+                <v-layout column  xs 12 sm12 lg6 class="me-2">
+                    <v-container fluid>
+                        <v-layout row xs12>
+
+                            <v-card height="300" width="100%">
+
+                                <v-card-title>
+                                    Announcement
+                                </v-card-title> 
+                            </v-card>
+                        </v-layout>
+
+                        <v-layout row xs12 class="mt-8">
+                            <v-card height="300" width="100%">
+                                <v-card-title>
+                                    To do
+                                </v-card-title> 
+                            </v-card>
+                        </v-layout>
+
+
+                    </v-container>
+
+
+
+                </v-layout>
+
+                <v-layout column xs12 sm12 lg6 class="ms-2">
+                    <v-card height="600">
+
+                        <v-card-title>
+                            Right
+                        </v-card-title>
+                    </v-card>
+                </v-layout>
+
+
+            </v-layout>
+            
+
+            
+        </v-container>
+
     </div>
 </template>
   
