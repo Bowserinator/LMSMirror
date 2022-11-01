26393d569a3e19783c3673ae8451a14d34425987
Ethan Tuttle <ethantut@gmail.com>
2022-10-31 21:02:42 -0400

    Create Discussion post page
    Also changed file name of discussion board file
    
    Co-Authored-By: Samuel Mei <43279636+SamooMei@users.noreply.github.com>

---
+++ frontend/pages/create-discussion-post.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="content">
+        <h1>Create Post</h1>
+        <v-divider class="dark my-5" color="white"></v-divider>
+        <v-card class="pa-6">
+            <v-text-field outlined style="max-width: 300px" label="Post Title"></v-text-field>
+            <v-textarea outlined auto-grow label="Post Contents"></v-textarea>
+            <div class="d-flex justify-end" extension extension-height="700">
+                <v-btn :ripple="false" dark class = "ma-2 pa-2">
+                    Cancel
+                </v-btn>
+                <v-btn :ripple="false" dark class = "ma-2 pa-2">
+                    Post
+                </v-btn>
+            </div>
+        </v-card>
+        
+    </div>
+</template>
+<script>
+  export default {
+      name: 'CreateDiscussionPost'
+  };
+</script>
+<style scoped>
+    .discussion_buttons {
+        margin-top: 50px;
+        margin-bottom: 50px;
+    }
+</style>
