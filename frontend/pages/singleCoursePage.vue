c3fe3a9eed1063f691da7c21c6c1f92f90c0cc1b
MeiH10 <huangm10@rpi.edu>
2022-10-31 20:21:36 -0400

    added a row with course name and refresh button

--- frontend/pages/singleCoursePage.vue
+++ frontend/pages/singleCoursePage.vue
@@ -1,6 +1,32 @@
 <template>
     <div>
-        
+
+        <v-container>
+
+            <v-layout row class="justify-space-between">
+
+                <v-flex sm11 md6 lg6>
+                    <PageHeader text="Linear Algebra" />
+                </v-flex>
+
+                <v-flex  sm1 md6 lg6 class="align-self-end">
+                    <v-btn class="float-right mb-2">
+                        <v-icon left>mdi-refresh</v-icon>
+                        <span class="text-capitalize">Refresh</span>
+                    </v-btn>
+                </v-flex>
+
+
+
+            </v-layout>
+
+
+        </v-container>
+        <!-- <PageHeader text="Linear Algebra" />
+        <v-btn>
+            <v-icon left>mdi-refresh</v-icon>
+            <span class="text-capitalize">Refresh</span>
+        </v-btn> -->
     </div>
 </template>
   
