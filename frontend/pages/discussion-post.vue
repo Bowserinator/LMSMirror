34f3086d752c3d9e7d1431d685de4568475c29b2
Ethan Tuttle <ethantut@gmail.com>
2022-11-07 23:27:25 -0500

    made posts on the board link to something now

---
+++ frontend/pages/discussion-post.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="content">
+        <h1>Discussion Post: [Thread]</h1>
+        <v-divider class="dark my-5" color="white"></v-divider>
+    </div>
+</template>
+<script>
+  export default {
+      name: 'DiscussionPost'
+  };
+</script>
+<style scoped>
+    
+</style>
