3c669fe4f6eb359c7bcd2ec31e261ffe62735464
Bowserinator <bowserinator@gmail.com>
2022-12-03 13:08:34 -0500

    Polish error page

--- frontend/layouts/error.vue
+++ frontend/layouts/error.vue
@@ -1,43 +1,59 @@
 <template>
-  <v-app dark>
-    <h1 v-if="error.statusCode === 404">
-      {{ pageNotFound }}
-    </h1>
-    <h1 v-else>
-      {{ otherError }}
-    </h1>
-    <NuxtLink to="/"> Home page </NuxtLink>
-  </v-app>
+    <v-app dark>
+        <div class="error-container">
+            <h1 v-if="error.statusCode === 404">
+                {{ pageNotFound }}
+            </h1>
+            <h1 v-else>
+                {{ error.statusCode + ': ' + otherError }}
+            </h1>
+
+            <p class="text--secondary mt-3">
+                If you think you found a bug, please add it to our 
+                <a href="https://github.com/Bowserinator/LMSMirror/issues">issue tracker</a> on github.
+            </p>
+
+            <v-btn to="/" class="mt-5">Back to Home Page</v-btn>
+        </div>
+    </v-app>
 </template>
 
 <script>
 export default {
-  name: "EmptyLayout",
-  layout: "empty",
-  props: {
-    error: {
-      type: Object,
-      default: null,
+    name: 'EmptyLayout',
+    layout: 'empty',
+    props: {
+        error: {
+            type: Object,
+            default: null,
+        },
     },
-  },
-  data() {
-    return {
-      pageNotFound: "404 Not Found",
-      otherError: "An error occurred",
+    data() {
+        return {
+            pageNotFound: '404 Not Found',
+            otherError: 'An error occurred',
+        }
+    },
+    head() {
+        const title = this.error.statusCode === 404 ?
+            this.pageNotFound : this.error.statusCode + ': ' + this.otherError
+        return { title };
     }
-  },
-  head() {
-    const title =
-      this.error.statusCode === 404 ? this.pageNotFound : this.otherError
-    return {
-      title,
-    }
-  },
 }
 </script>
 
 <style scoped>
+.error-container {
+    position: absolute;
+    top: 200px;
+    left: 50%;
+    text-align: center;
+    transform: translateX(-50%);
+    width: 300px;
+    max-width: 100%;
+}
+
 h1 {
-  font-size: 20px;
+    font-size: 40px;
 }
 </style>
